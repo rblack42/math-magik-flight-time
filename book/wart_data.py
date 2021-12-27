f304b8ea2cee2215b77f74f19c2986599cb2985f
Roie Black <roie.black@gmail.com>
2021-12-26 18:46:03 -0600

    added model data page

---
+++ book/wart_data.py
@@ -0,0 +1,9 @@
+import pint
+u = pint.UnitRegistry()
+airframe_weight = 1.2 * u.grams
+wing_surface_area = 20 * u.inch**2
+wart_motor_weight = 0.86 * u.grams
+wart_best_time = 618 * u.seconds
+wart_turns = 3660
+site_height = 147 * u.feet
+site_elevation = 2169 * u.feet
